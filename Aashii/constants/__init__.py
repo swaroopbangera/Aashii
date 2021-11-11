4a63b7d556aa08da9269e61bd95bd439f68dae5f
Suhasa SJ <suhasasj@gmail.com>
2021-11-11 15:33:04 +0530

    Delete Aashii directory

--- Aashii/constants/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Collection of constant objects."""
-
-from .button import Button
-from .label import Label
-from .literal import Literal
-from .message import Message
-from .query import Query
-from .scope import Scope
